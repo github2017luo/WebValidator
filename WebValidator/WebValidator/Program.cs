9362cc276698765e42ab06df2aee0c1ba3a85149
Jonah <jonasz.karafiol@gmail.com>
2019-12-06 19:11:25 +0100

    Expanded validatior

--- WebValidator/WebValidator/Program.cs
+++ WebValidator/WebValidator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
 using WebValidator.Logger;
 using WebValidator.Search;
 using WebValidator.SeleniumDriver;
@@ -8,11 +11,27 @@ namespace WebValidator
     internal class Program
     {
         private static readonly ILogger Logger = new ConsoleLogger();
+        private static string _browser;
         private static void Main(string[] args)
         {
-            using var driver = new SeleniumDriverInitilizer().InitializeDriver(args[0]);
+            _browser = args[0];
+            var driver = ValidatePage(_browser, args[1]);
+            var elements = new SearchElements(driver).GetBy(By.XPath(".//*[@href]"));
+            var urls = new List<string>();
+            foreach (var webElement in elements)
+            {
+                urls.Add(webElement.GetAttribute("href"));
+            }
+            driver.Dispose();
+            Parallel.ForEach(urls, url => { ValidatePage(_browser, url).Dispose(); });
+        }
+
+        private static IWebDriver ValidatePage(string browser, string url)
+        {
+            Logger.Log("Validation of " + url);
+            var driver = new SeleniumDriverInitilizer().InitializeDriver(browser);
             var validator = new Validator.Validator(driver,
-                new Uri(args[1]),
+                new Uri(url),
                 new SearchElements(driver),
                 new Request.Request(),
                 5,
@@ -23,6 +42,7 @@ namespace WebValidator
             Logger.Log(DateTime.Now + " Checking images.");
             errors = validator.ValidateImages();
             Logger.LogErrors(errors);
+            return driver;
         }
     }
 }
--- WebValidator/WebValidator/Validator/Validator.cs
+++ WebValidator/WebValidator/Validator/Validator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,37 +16,84 @@ namespace WebValidator.Validator
         private readonly ISearchElements _search;
         private readonly IRequest _request;
         private readonly ILogger _logger;
+        private static ConcurrentDictionary<string, bool> _visitedPages;
 
         public Validator(IWebDriver driver, Uri uri, ISearchElements search, IRequest request, int waitSeconds, ILogger logger)
         {
             _search = search;
             _request = request;
             _logger = logger;
+            _visitedPages ??= new ConcurrentDictionary<string, bool>();
             driver.Navigate().GoToUrl(uri);
+            Wait(waitSeconds);
+        }
+
+        private void Wait(int waitSeconds)
+        {
+            if (waitSeconds <= 0)
+            {
+                return;
+            }
             _logger.Log($"Waiting {waitSeconds} seconds.");
             Thread.Sleep(waitSeconds * 1000);
         }
+
         public List<ErrorDto> ValidateUrls()
         {
-            var elements = _search.GetBy(By.XPath(".//*[@href]"));
+            var elements = _search.GetBy(By.XPath(".//a[@href]"));
             _logger.Log($"Found {elements.Count} urls.");
             var errorList = new List<ErrorDto>();
-            Parallel.ForEach(elements, webElement =>
+            //Parallel.ForEach(elements, webElement =>
+            foreach (var webElement in elements)
             {
-                var uri = new Uri(webElement.GetAttribute("href"));
-                var status = _request.SendHeadRequest(uri);
-                if (status >= 300 && status <= 599)
+                string uri;
+                try
                 {
-                    errorList.Add(new ErrorDto
-                    {
-                        Element = webElement,
-                        StatusCode = status,
-                        Uri = uri
-                    });
+                    uri = webElement.GetAttribute("href");
                 }
-            });
+                catch (UriFormatException)
+                {
+                    AddError(errorList, webElement, 404, null);
+                    continue;//return;
+                }
+
+                _logger.Log("Working on: " + uri);
+
+                if (AlreadyVisited(uri)) continue;//return;
+
+                var status = _request.SendHeadRequest(new Uri(uri));
+                if (IsErrorStatus(status))
+                {
+                    AddError(errorList, webElement, status, uri);
+                }
+
+                //TODO: Handle the false case?
+                if (_visitedPages.TryAdd(uri, true))
+                    _logger.Log("Added " + uri);
+            }//);
             return errorList;
         }
+
+        private static bool IsErrorStatus(int status)
+        {
+            return status >= 300 && status <= 599;
+        }
+
+        private static bool AlreadyVisited(string uri)
+        {
+            return _visitedPages.ContainsKey(uri) && _visitedPages[uri];
+        }
+
+        private static void AddError(List<ErrorDto> errorList, IWebElement webElement, int status, string uri)
+        {
+            errorList.Add(new ErrorDto
+            {
+                Element = webElement,
+                StatusCode = status,
+                Uri = uri
+            });
+        }
+
 
         // TODO: Check how it behaves with base 64 encoded content. 
         public List<ErrorDto> ValidateImages()
@@ -56,8 +104,8 @@ namespace WebValidator.Validator
             var errorList = new List<ErrorDto>();
             Parallel.ForEach(elements, webElement =>
             {
-                var uri = new Uri(webElement.GetAttribute("src"));
-                var status = _request.SendHeadRequest(uri);
+                var uri =webElement.GetAttribute("src");
+                var status = _request.SendHeadRequest(new Uri(uri));
                 if (status >= 300 && status <= 599)
                 {
                     errorList.Add(new ErrorDto
@@ -70,5 +118,7 @@ namespace WebValidator.Validator
             });
             return errorList;
         }
+
+
     }
 }
