d4f6cfd4ea221d0ab054e95be4412ef29e16e5d5
Jonah <jonasz.karafiol@gmail.com>
2020-02-05 21:04:19 +0100

    Added Node class

--- WebValidator/WebValidator/Node.cs
+++ WebValidator/WebValidator/Node.cs
@@ -4,8 +4,40 @@ namespace WebValidator
 {
     public class Node
     {
-        private List<string> _parentNodes;
-        private List<string> _childrenNodes;
-        private string _path;
+        private readonly HashSet<string> _parentNodes;
+        private readonly string _path;
+        private bool _visited;
+
+        public Node(string path)
+        {
+            _path = path;
+            _parentNodes = new HashSet<string>();
+        }
+
+        public Node MakeVisited()
+        {
+            _visited = true;
+            return this;
+        }
+
+        public bool GetVisited()
+        {
+            return _visited;
+        }
+
+        public Node AddParentNode(string url)
+        {
+            lock (_parentNodes)
+            {
+                _parentNodes.Add(url);
+            }
+
+            return this;
+        }
+
+        public string GetPath()
+        {
+            return _path;
+        }
     }
 }
