b978bce95dfdbb05e18c61365ee3e6c5dead0005
Jonah <jonasz.karafiol@gmail.com>
2020-02-08 12:53:52 +0100

    New Node methods

--- WebValidator/WebValidator/Node.cs
+++ WebValidator/WebValidator/Node.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 
 namespace WebValidator
 {
     public class Node
     {
         private readonly HashSet<string> _parentNodes;
-        private readonly string _path;
+        private readonly ICollection<string> _errors;
+        private readonly string _url;
         private bool _visited;
+        private HttpStatusCode _statusCode;
 
-        public Node(string path)
+        public Node(string url)
         {
-            _path = path;
+            _url = url;
             _parentNodes = new HashSet<string>();
+            _errors = new List<string>();
         }
 
         public Node MakeVisited()
@@ -35,9 +39,9 @@ namespace WebValidator
             return this;
         }
 
-        public string GetPath()
+        public string GetUrl()
         {
-            return _path;
+            return _url;
         }
 
         public ICollection<string> GetParentNodes()
@@ -46,6 +50,35 @@ namespace WebValidator
             {
                 return _parentNodes;
             }
+        }
+
+        public Node AddError(string error)
+        {
+            lock (_errors)
+            {
+                _errors.Add(error); 
+            }
+
+            return this;
+        }
+
+        public IEnumerable<string> GetErrors()
+        {
+            lock (_errors)
+            {
+                return _errors;
+            }
+        }
+
+        public Node SetStatusCode(HttpStatusCode status)
+        {
+            _statusCode = status;
+            return this;
+        }
+
+        public HttpStatusCode GetStatusCode()
+        {
+            return _statusCode;
         }
     }
 }
