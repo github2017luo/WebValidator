13891291c781bf0a56875f3c4a6e1149f9388786
Jonah <jonasz.karafiol@gmail.com>
2020-02-08 12:53:37 +0100

    Updated dtos used in json

---
+++ WebValidator/WebValidator/Json/NodeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace WebValidator.Json
+{
+    public class NodeDto
+    {
+        public List<string> ParentNodes;
+        public string Url { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
