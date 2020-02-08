a5a738c3c6fa6511030ff00fd3c01111106476e1
Jonah <jonasz.karafiol@gmail.com>
2020-02-08 12:56:46 +0100

    Extracted method, rewritten json save

--- WebValidator/WebValidator/Program.cs
+++ WebValidator/WebValidator/Program.cs
@@ -21,21 +21,30 @@ namespace WebValidator
             var watch = Stopwatch.StartNew();
             var crawler = new HtmlCrawler(logger, _depth, _baseUrl);
             crawler.Crawl(0,_baseUrl);
-            var pages = crawler.GetVisitedPages();
+            var pages = crawler.GetPages();
             watch.Stop();
 
-            foreach (var error in crawler.GetErrors())
-            {
-                logger.Log($"{error.Key} {error.Value}");
-            }
+            LogCrawlErrors(pages, logger);
 
             logger.Log($"Found: {pages.Count}");
             logger.Log("Search time: " + watch.Elapsed);
 
-            //SaveToJson(pages);
-
+            SaveToJson(pages);
 
             ////file:///D:\\Studia\\WebValidator\\Web\\Web1.html
+        }
+
+        private static void LogCrawlErrors(IReadOnlyDictionary<string, Node> pages, ILogger logger)
+        {
+            foreach (var (_, node) in pages)
+            {
+                var errors = node.GetErrors();
+                foreach (var error in errors)
+                {
+                    logger.Log("Url: " + node.GetUrl());
+                    logger.Log(error);
+                }
+            }
         }
 
         private static void InitConfig()
@@ -46,17 +55,37 @@ namespace WebValidator
             _depth = config.Depth;
         }
 
-        private static void SaveToJson(IReadOnlyDictionary<string, bool> pages)
+        private static void SaveToJson(IReadOnlyDictionary<string, Node> pages)
         {
-            var keys = pages.Keys.ToList();
-            keys.Sort();
-
-            var dto = new VisitedPagesDto
+            var nodes = pages.Values.OrderBy(p => p.GetUrl());
+            var nodesDto = new VisitedPagesDto()
             {
-                Pages = keys
+                Pages = new List<NodeDto>()
             };
 
-            File.WriteAllText(Directory.GetCurrentDirectory() + @"\json.json", new SaveToJson().Serialize(dto));
+            foreach (var node in nodes)
+            {
+                List<string> parentNodes;
+                if ((int)node.GetStatusCode() >= 200 && (int)node.GetStatusCode() <= 299)
+                {
+                    parentNodes = new List<string>();
+                }
+                else
+                {
+                    parentNodes = node.GetParentNodes().ToList();
+                }
+
+
+                nodesDto.Pages.Add(new NodeDto
+                {
+                    ParentNodes = parentNodes,
+                    StatusCode = node.GetStatusCode(),
+                    Url = node.GetUrl()
+                });
+            }
+            
+
+            File.WriteAllText(Directory.GetCurrentDirectory() + @"\json.json", new SaveToJson().Serialize(nodesDto));
         }
 
         //private static IEnumerable<string> Check(string url)
