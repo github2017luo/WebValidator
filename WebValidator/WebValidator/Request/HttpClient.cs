0ca262f22bb3a397f9f8ddec70dacc73ddddbd95
Jonah <jonasz.karafiol@gmail.com>
2020-02-09 13:48:45 +0100

    Renamed

--- WebValidator/WebValidator/Request/HttpClient.cs
+++ WebValidator/WebValidator/Request/HttpClient.cs
@@ -1,26 +1,27 @@
 ﻿using System;
+using System.Net;
 using RestSharp;
 
 namespace WebValidator.Request
 {
-    public class Request : IRequest
+    public class HttpClient : IHttpClient
     {
-        public int SendHeadRequest(Uri uri) 
+        public HttpStatusCode SendHeadRequest(Uri uri) 
         {
             var client = new RestClient(uri);
             var request = new RestRequest(Method.HEAD);
 
             var response = client.Execute(request);
-            return (int)response.StatusCode;
+            return response.StatusCode;
         }
 
-        public int SendGetRequest(Uri uri)
+        public HttpStatusCode SendGetRequest(Uri uri)
         {
             var client = new RestClient(uri);
             var request = new RestRequest(Method.GET);
 
             var response = client.Execute(request);
-            return (int)response.StatusCode;
+            return response.StatusCode;
         }
     }
 }
---
+++ WebValidator/WebValidator/Request/IHttpClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net;
+
+namespace WebValidator.Request
+{
+    public interface IHttpClient
+    {
+        HttpStatusCode SendHeadRequest(Uri uri);
+        HttpStatusCode SendGetRequest(Uri uri);
+    }
+}
